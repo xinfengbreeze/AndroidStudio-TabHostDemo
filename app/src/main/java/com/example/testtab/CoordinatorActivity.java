4229d30cb4a7c8a4a602dc829be9df12e6041223
kaifeng.yuan <772890398@qq.com>
2016-12-01 22:06:57 +0800

    加入CoordinatorLayout+AppbarLayout+ToolBar+TabLayout 各个子View之间相互协调滑动

---
+++ app/src/main/java/com/example/testtab/CoordinatorActivity.java
@@ -0,0 +1,24 @@
+package com.example.testtab;
+
+import android.support.design.widget.TabItem;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.testtab.model.TabItemViewHolder;
+
+public class CoordinatorActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coordinator);
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setCustomView(new TabItemViewHolder(this,"视频",R.drawable.tab_icon_selector).getTabView()));
+        tabLayout.addTab(tabLayout.newTab().setCustomView(new TabItemViewHolder(this,"音乐",R.drawable.tab_icon_selector).getTabView()));
+        tabLayout.addTab(tabLayout.newTab().setCustomView(new TabItemViewHolder(this,"美食",R.drawable.tab_icon_selector).getTabView()));
+        tabLayout.addTab(tabLayout.newTab().setCustomView(new TabItemViewHolder(this,"游戏",R.drawable.tab_icon_selector).getTabView()));
+    }
+}
--- app/src/main/java/com/example/testtab/ui/fragment/SettingPager.java
+++ app/src/main/java/com/example/testtab/ui/fragment/SettingPager.java
@@ -1,5 +1,6 @@
 package com.example.testtab.ui.fragment;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -7,8 +8,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.testtab.CoordinatorActivity;
 import com.example.testtab.R;
 
 /**
@@ -18,6 +21,8 @@ import com.example.testtab.R;
 public class SettingPager extends Fragment {
     private View  rootView;
     private TextView tv;
+    private Button btCoordinator;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -27,6 +32,13 @@ public class SettingPager extends Fragment {
     public View initView(){
         if(rootView == null){
             rootView = View.inflate(getActivity(), R.layout.fragment_setting_pager,null);
+            btCoordinator = (Button) rootView.findViewById(R.id.bt_coordinatorLayout);
+            btCoordinator.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(getContext(),CoordinatorActivity.class));
+                }
+            });
             tv = (TextView) rootView.findViewById(R.id.textView);
             tv.setText(this.getClass().getSimpleName());
         }else{
@@ -38,5 +50,6 @@ public class SettingPager extends Fragment {
         }
         return rootView;
     }
+
 
 }
